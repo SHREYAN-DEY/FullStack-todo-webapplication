14822589943b87c920a29edf46641e02c9b623c8
Shreyan Dey <shreyandey1234@gmail.com>
2026-07-29 16:52:18 +0530

    modify App.jsx

--- week2/smartToDo/smartToDo/src/App.jsx
+++ week2/smartToDo/smartToDo/src/App.jsx
@@ -1,5 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import AddTask from "./pages/AddTask";
+import TaskDetails from "./pages/TaskDetails";
+import Profile from "./pages/Profile";
+
 function App() {
-  return <h1>Smart To Do</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/add-task" element={<AddTask />} />
+        <Route path="/task/:id" element={<TaskDetails />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
